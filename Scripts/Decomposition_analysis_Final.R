1125964f9192da22023cc3359d68085ab817a6b3
mmccar26 <matt.mccary@gmail.com>
2020-09-08 00:08:19 -0400

    Add files via upload

---
+++ Scripts/Decomposition_analysis_Final.R
@@ -0,0 +1,74 @@
+#McCary and Schmitz functional traits analysis
+#7 Sept. 2020
+
+#=======Litter decomposition==========
+
+#===load libraries======
+library(ggplot2)
+library(metafor)
+library(plyr)
+library(lme4)
+library(car)
+library(dplyr)
+library(tidyverse)
+library(broom)
+library(Rmisc)
+
+#=====import data======
+#relative pathname
+meta <- file.path(".", "Data", "Decomposition_Data.csv")
+print(meta)
+
+#import data
+decomp <- read_csv(meta)%>%
+  filter(Units == "prop. decom" & 
+           Trait != "Leaf-piercer")%>% ##not enough leaf piercers in data set
+           drop_na(Std.dev.control)
+
+#Calculate log Response Ratio
+decomp.es<-escalc(measure="ROM",
+            m1i= Mean.control, m2i=Mean.experimental,
+            sd1i=Std.dev.control, sd2i=Std.dev.experimental,
+            n1i=SS.control, n2i= SS.experimental, 
+            data=decomp,
+            var.names=c("LRR","LRR_var"),digits=4)
+
+##to remove outliers for portion decomposed (Ploss > 3 SD)
+isnt_out_mad <- function(x, thres = 3, na.rm = TRUE) {
+  abs(x - median(x, na.rm = na.rm)) <= thres * mad(x, na.rm = na.rm)
+}
+
+lit <- 
+  decomp.es%>%
+  mutate(Outlier = isnt_out_mad(decomp.es$LRR))%>%
+  filter(Outlier == "TRUE")
+
+#==========Random-effects model=================
+mod0<-rma(yi=LRR,vi=LRR_var, data=lit)
+summary(mod0)
+
+#==========Mixed-effects model==================
+mod1<-rma.mv(yi=LRR,V=LRR_var, mods=~factor(Trait)-1, random = list(~1|ES.ID, ~1|StudyID),
+             struct="CS",method="REML",digits=4,level = 95, 
+             data=lit)
+summary(mod1)
+
+#===========Model fit diagnostics===============
+par(mfrow=c(1,1)) ## set the plot matrix
+
+##qq norm plots
+qqPlot(residuals.rma(mod1), xlab="Theoretical Quantiles", ylab = "Sample Quantiles", line = "quartiles", col.lines = "black", grid = FALSE)
+
+##residual vs fitted plot
+plot(fitted.rma(mod1), residuals.rma(mod1), xlab = "Fitted Residuals", ylab = "Residuals") #residuals vs fitted
+abline(h=0)
+
+#==========Publication bias analyses====================
+#Trim and fill method
+res <- rma(LRR, LRR_var, data = lit, method = "FE")
+trimfill(res)
+funnel(res, legend = TRUE, size =4 )
+
+#Rosenthal's fail-safe number
+fsn(LRR, LRR_var, data = lit, type = "Rosenthal")
+
